90f615ebcf3be94b22610789d9c2c933122484c2
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-06-05 03:24:05 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,20 @@
-// Code MovieReviews Here
+import React from 'react'
+
+export default function MovieReviews(props){
+
+    return(
+        <div className="review-list">
+            <ul>
+                {props.reviews.map(r => {
+                    return (
+                        <li key={r.link.url} className="review">
+                            <h3>{r.headline} | Opens: {r.opening_date}</h3>
+                            <p>"{r.summary_short}"</p>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+
+}
